da6905e04080dc2796bba311328369e876986e79
Connor Ginty <gintycd@gmail.com>
2022-08-11 10:43:54 -0400

    multiple genres on band

--- capstone/vue/src/components/NewBandForm.vue
+++ capstone/vue/src/components/NewBandForm.vue
@@ -1,46 +1,63 @@
 <template>
   <form class="new-band-form" v-on:submit.prevent="saveBand">
-    <input
-      class="band-name-input"
-      type="text"
-      placeholder="Band Name"
-      v-model="newBand.bandName"
-    />
-    <input
-      class="band-description-input"
-      type="text"
-      placeholder="Your Band's Description"
-      v-model="newBand.description"
-    />
-    <select name="genre-selector" id="genre-select" v-model="newBand.genre">
-      <option value="Pop">Pop</option>
-      <option value="Rock">Rock</option>
-      <option value="HipHop">HipHop</option>
-      <option value="Classical">Classical</option>
-      <option value="RnB">{{ "R & B" }}</option>
-      <option value="Electronic">Electronic</option>
-      <option value="Country">Country</option>
-      <option value="Blues">Blues</option>
-      <option value="Jazz">Jazz</option>
-      <option value="Metal">Metal</option>
-      <option value="Reggae">Reggae</option>
-      <option value="Folk">Folk</option>
-      <option value="Latin">Latin</option>
-      <option value="A capella">A capella</option>
-      <option value="Funk">Funk</option>
-      <option value="Worship">Worship</option>
-      <option value="Instrumental">Instrumental</option>
-      <option value="Punk">Punk</option>
-      <option value="Singer/Songwriter">Singer/Songwriter</option>
-      <option value="World">World</option>
-    </select>
-    <input
-      class="image-link"
-      type="text"
-      placeholder="Image Link"
-      v-model="newBand.imageLink"
-    />
-    <button class="button">Save</button>
+    <ul class="band-creation">
+      <li>
+        <input
+          class="band-name-input"
+          type="text"
+          placeholder="Band Name"
+          v-model="newBand.bandName"
+        />
+      </li>
+      <li>
+        <textarea
+          class="band-description-input"
+          type="textarea"
+          placeholder="Your Band's Description"
+          v-model="newBand.description"
+        >
+        </textarea>
+      </li>
+      <li>
+        <select
+          name="genre-selector"
+          id="genre-select"
+          v-model="newBand.genre"
+          multiple
+        >
+          <option value="" disabled selected>Select Genre</option>
+          <option value="Pop">Pop</option>
+          <option value="Rock">Rock</option>
+          <option value="HipHop">HipHop</option>
+          <option value="Classical">Classical</option>
+          <option value="RnB">{{ "R & B" }}</option>
+          <option value="Electronic">Electronic</option>
+          <option value="Country">Country</option>
+          <option value="Blues">Blues</option>
+          <option value="Jazz">Jazz</option>
+          <option value="Metal">Metal</option>
+          <option value="Reggae">Reggae</option>
+          <option value="Folk">Folk</option>
+          <option value="Latin">Latin</option>
+          <option value="A capella">A capella</option>
+          <option value="Funk">Funk</option>
+          <option value="Worship">Worship</option>
+          <option value="Instrumental">Instrumental</option>
+          <option value="Punk">Punk</option>
+          <option value="Singer/Songwriter">Singer/Songwriter</option>
+          <option value="World">World</option>
+        </select>
+      </li>
+      <li>
+        <textarea
+          class="image-link"
+          placeholder="Image Link"
+          v-model="newBand.imageLink"
+        >
+        </textarea>
+      </li>
+      <button class="button">Save Band</button>
+    </ul>
   </form>
 </template>
 
@@ -80,4 +97,8 @@ export default {
 /* .button {
   font-size: 200px;
 } */
+.band-creation,
+.band-name-input {
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+}
 </style>
