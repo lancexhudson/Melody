7f81d21722b6dde15ded71d87964d1b8bbc8064b
Connor Ginty <gintycd@gmail.com>
2022-08-11 16:41:50 -0400

    band page working

--- capstone/vue/src/components/BandDetails.vue
+++ capstone/vue/src/components/BandDetails.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <h1 class="band-name">{{ band.bandName }}</h1>
+    <img :src="band.imageLink" class="hero-picture" />
+    <h4 class="genre">Genre: {{ band.genre }}</h4>
+    <p class="description">{{ band.description }}</p>
   </div>
 </template>
 
@@ -13,5 +16,25 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style>
+body.bandDetails {
+  display: flex;
+  text-align: center;
+  flex-direction: column;
+  justify-content: flex-center;
+
+  align-content: center;
+  background: black;
+  color: white;
+  background-image: url("~@/assets/crowd.png");
+  background-position: bottom;
+  background-size: cover;
+  background-attachment: fixed;
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+  height: 100vh;
+}
+.hero-picture {
+  max-height: 500px;
+  max-width: 500px;
+}
 </style>
