2a751b76805ff5df2b18027d804a7c0dcd3f4ca3
Patrick Morris <pnmorris513@gmail.com>
2022-08-11 10:41:41 -0400

    search working

--- capstone/vue/src/components/BandList.vue
+++ capstone/vue/src/components/BandList.vue
@@ -1,8 +1,19 @@
 <template>
   <div class="band-container">
+    <table id="tableBands">
+      <thead>
+        <tr>
+          <th>Band Name</th>
+          <!-- <th>Genre</th> -->
+        </tr>
+      </thead>
+      <td>
+        <input type="text" id="bandNameFilter" v-model="filter.bandName" />
+      </td>
+    </table>
     <band-card
       v-bind:band="band"
-      v-for="band in $store.state.bands"
+      v-for="band in filteredBandList"
       v-bind:key="band.id"
     >
     </band-card>
@@ -16,6 +27,27 @@ export default {
   name: "band-list",
   components: {
     bandCard,
+  },
+  data() {
+    return {
+      filter: {
+        bandName: "",
+        // genreFilter: "",
+      },
+    };
+  },
+  computed: {
+    filteredBandList() {
+      let filteredBands = this.$store.state.bands;
+      if (this.filter.bandName != "") {
+        filteredBands = filteredBands.filter((band) =>
+          band.bandName
+            .toLowerCase()
+            .includes(this.filter.bandName.toLowerCase())
+        );
+      }
+      return filteredBands;
+    },
   },
   methods: {
     listBands() {
