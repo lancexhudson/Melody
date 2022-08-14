787ffcf0e9a0bd6c62550b9ab9a6974d68d8acaa
Michael Felger <michaelfelger@gmail.com>
2022-08-14 15:11:12 -0400

    actually saved this time

--- capstone/java/database/melody.sql
+++ capstone/java/database/melody.sql
@@ -1,5 +1,5 @@
 BEGIN TRANSACTION;
-DROP TABLE IF EXISTS band, genre, band_genre, band_user;
+DROP TABLE IF EXISTS band, genre, band_genre, band_user, user_favorite_bands;
 DROP SEQUENCE IF EXISTS seq_band_id, seq_genre_id;
 CREATE SEQUENCE seq_band_id
   INCREMENT BY 1
@@ -41,19 +41,6 @@ user_id int NOT NULL,
 CONSTRAINT PK_band_user_band_id PRIMARY KEY (band_id),
 CONSTRAINT FK_band_id FOREIGN KEY (band_id) REFERENCES band(band_id),
 CONSTRAINT FK_user_id FOREIGN KEY (user_id) REFERENCES users(user_id)
-);
-CREATE TABLE message (
-message_id serial,
-to_user int NOT NULL,
-from_band int NOT NULL,
-message varchar(144) NOT NULL,
-date_received timestamp NOT NULL,
-
-CONSTRAINT PK_message_id PRIMARY KEY (message_id),
-CONSTRAINT FK_to_user FOREIGN KEY (to_user) REFERENCES users(user_id),
-CONSTRAINT FK_from_band FOREIGN KEY (from_band) REFERENCES band(band_id)
-);
-
 );
 
 INSERT INTO genre (genre_name) 
