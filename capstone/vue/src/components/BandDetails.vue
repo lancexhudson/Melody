5f423b338817cde8cbdac9b2e575b2aaa50042ac
Lance Hudson <lancexhudson@gmail.com>
2022-08-16 16:07:55 -0400

    afternoon styling lh

--- capstone/vue/src/components/BandDetails.vue
+++ capstone/vue/src/components/BandDetails.vue
@@ -143,7 +143,6 @@ body.bandDetails {
 .upcoming-events-h3 {
   text-decoration: underline;
   font-weight: lighter;
-
   margin-bottom: 0px;
 }
 
@@ -152,7 +151,6 @@ body.bandDetails {
   border-radius: 0.5rem;
   border: solid rgb(255, 255, 255) 1px;
   background: rgba(0, 0, 0, 0.8);
-
   margin: 1rem;
   margin: 25px 0;
   font-size: 0.9em;
@@ -164,7 +162,7 @@ body.bandDetails {
 }
 
 .upcoming-events-table thead tr {
-  background-color: rgba(0, 0, 0, 0.8);
+  background-color: rgb(29, 26, 26);
   color: #ffffff;
   text-align: center;
 }
