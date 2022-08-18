bac6a7570d7d4d30ece0d837811c50dbc3c002c1
Michael Felger <michaelfelger@gmail.com>
2022-08-18 10:38:03 -0400

    Autofill updated with more bands

--- capstone/java/database/melody.sql
+++ capstone/java/database/melody.sql
@@ -165,9 +165,64 @@ VALUES ('Daft Punk', 'Daft Punk were a French electronic music duo formed in 199
 INSERT INTO band_genre (band_id, genre_id)
 VALUES (1011, 2006);
 
+INSERT INTO band (band_name, description, image_link)
+VALUES ('Siouxsie and the Banshees', 'Siouxsie and the Banshees were a British rock band, formed in London in 1976 by vocalist Siouxsie Sioux and bass guitarist Steven Severin. They have been widely influential, both over their contemporaries and with later acts. Q magazine included John McKay''s guitar playing on Hong Kong Garden in their list of 100 Greatest Guitar Tracks Ever, while Mojo rated guitarist John McGeoch in their list of 100 Greatest Guitarists of All Time for his work on Spellbound. The Times called the group one of the most audacious and uncompromising musical adventurers of the post-punk era.',
+'https://upload.wikimedia.org/wikipedia/en/6/6f/Siouxsie_and_the_banshees_79.jpg');
+INSERT INTO band_genre (band_id, genre_id)
+VALUES (1012, 2002);
 
+INSERT INTO band (band_name, description, image_link)
+VALUES ('Willie Nelson', 'Willie Hugh Nelson (born April 29, 1933) is an American country musician. The critical success of the album Shotgun Willie (1973), combined with the critical and commercial success of Red Headed Stranger (1975) and Stardust (1978), made Nelson one of the most recognized artists in country music. He was one of the main figures of outlaw country, a subgenre of country music that developed in the late 1960s as a reaction to the conservative restrictions of the Nashville sound',
+		'https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Willie_Nelson%2C_Fort_Wayne%2C_Indiana_%28June_27%2C_2018%29_-_Pamela_Smith_06.jpg/1280px-Willie_Nelson%2C_Fort_Wayne%2C_Indiana_%28June_27%2C_2018%29_-_Pamela_Smith_06.jpg');
+INSERT INTO band_genre (band_id, genre_id)
+VALUES (1013, 2007);
 
+INSERT INTO band (band_name, description, image_link)
+VALUES ('Erykah Badu', 'Erica Abi Wright (born February 26, 1971), known professionally as Erykah Badu, is an American singer-songwriter, record producer and actress. Influenced by R&B, 1970s soul, and 1980s hip hop, Badu became associated with the neo soul subgenre in the 1990s and 2000s along with artists such as D''Angelo and Maxwell. She has been called the ''Queen of Neo Soul''. Badu''s career began after she opened a show for D''Angelo in 1994 in Fort Worth; record label executive Kedar Massenburg was highly impressed with her performance and signed her to Kedar Entertainment. Her first album, Baduizm, was released in February 1997. It spawned four singles: ''On & On'', ''Appletree'', ''Next Lifetime'' and ''Otherside of the Game.'' The album was certified triple Platinum by the Recording Industry Association of America (RIAA). Her first live album, Live, was released in November 1997 and was certified double Platinum by the RIAA.',
+'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/Erykah_badu-02-mika.jpg/1024px-Erykah_badu-02-mika.jpg');
+INSERT INTO band_genre (band_id, genre_id)
+VALUES (1014, 2005);
 
+INSERT INTO band (band_name, description, image_link)
+VALUES ('Albert King', 'Albert Nelson (April 25, 1923 – December 21, 1992), known by his stage name Albert King, was an American blues guitarist and singer whose playing influenced many other blues guitarists. He is perhaps best known for the popular and influential album Born Under a Bad Sign (1967) and its title track. He, B.B. King, and Freddie King, all unrelated, were known as the ''Kings of the Blues''. The left-handed King was known for his ''deep, dramatic sound that was widely imitated by both blues and rock guitarists.''',
+'https://upload.wikimedia.org/wikipedia/commons/3/30/Albert_King.jpg');
+INSERT INTO band_genre (band_id, genre_id)
+VALUES (1015, 2008);
+
+INSERT INTO band (band_name, description, image_link)
+VALUES ('Culture', 'Culture are a Jamaican roots reggae group founded in 1976. Originally they were known as the African Disciples. The one constant member until his death in 2006 was Joseph Hill.',
+'https://wildharemusic.com/wp-content/uploads/2014/09/Culture.jpg');
+INSERT INTO band_genre (band_id, genre_id)
+VALUES (1016, 2011);
+
+INSERT INTO band (band_name, description, image_link)
+VALUES ('Juanes', 'Juan Esteban Aristizábal Vásquez (born August 9, 1972), known professionally as Juanes, is a Colombian musician who was a member of the rock band Ekhymosis and is now a solo artist. In 2000, his solo debut album Fíjate Bien won three Latin Grammy Awards. According to his record label, Juanes has sold more than 15 million albums worldwide.',
+'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/Juanes_FPML.JPG/1920px-Juanes_FPML.JPG');
+INSERT INTO band_genre (band_id, genre_id)
+VALUES (1017, 2013);
+
+INSERT INTO band (band_name, description, image_link)
+VALUES ('Rick James', 'James Ambrose Johnson Jr. (February 1, 1948 – August 6, 2004), better known by his stage name Rick James, was an American singer-songwriter, musician and record producer. Born and raised in Buffalo, New York, James began his musical career in his teenage years. He was in various bands before entering the U.S. Navy Reserve to avoid being drafted into the army. In 1964, James deserted to Toronto, Canada, where he formed the rock band the Mynah Birds, who eventually signed a recording deal with Motown Records in 1966. James'' career with the group halted after military authorities discovered his whereabouts and eventually convicted and sentenced James to a one-year prison term related to the desertion charges. After being released, James moved to California, where he started a variety of rock and funk groups in the late 1960s and early 1970s.',
+'https://allthatsinteresting.com/wordpress/wp-content/uploads/2021/08/portrait-of-rick-james.jpg');
+INSERT INTO band_genre (band_id, genre_id)
+VALUES (1018, 2015);
+
+INSERT INTO band (band_name, description, image_link)
+VALUES ('Milt Jackson', 'Milton Jackson (January 1, 1923 – October 9, 1999), nicknamed ''Bags'', was an American jazz vibraphonist, usually thought of as a bebop player, although he performed in several jazz idioms. He is especially remembered for his cool swinging solos as a member of the Modern Jazz Quartet and his penchant for collaborating with hard bop and post-bop players.',
+'https://rnz-ressh.cloudinary.com/image/upload/s--e3bL4MRO--/c_scale,f_auto,q_auto,w_1050/4N3FP6H_image_crop_90605');
+INSERT INTO band_genre (band_id, genre_id)
+VALUES (1019, 2009);
+
+INSERT INTO band (band_name, description, image_link)
+VALUES ('Dirty Three', 'Dirty Three is an Australian instrumental rock band, consisting of Warren Ellis (violin and bass guitar), Mick Turner (electric and bass guitars) and Jim White (drums), which formed in 1992. Their 1996 album Horse Stories was voted by Rolling Stone as one of the top three albums of the year. Two of their albums have peaked into the top 50 on the ARIA Albums Chart, Ocean Songs (1998) and Toward the Low Sun (2012). During their career they have spent much of their time overseas when not performing together. Turner is based in Melbourne, White lives in New York, and Ellis in Paris. Australian rock music historian Ian McFarlane described them as providing a ''rumbling, dynamic sound incorporated open-ended, improvisational, electric rock ... minus the jazz-rock histrionics''. In October 2010, Ocean Songs was listed in the book 100 Best Australian Albums.',
+'https://media.npr.org/assets/img/2012/02/13/dirtythree-a9f5f46f62c84abd1de3ea4c6a1ac7696a61219d-s300-c85.webp');
+INSERT INTO band_genre (band_id, genre_id)
+VALUES (1020, 2017);
+
+--INSERT INTO band (band_name, description, image_link)
+--VALUES ();
+--INSERT INTO band_genre (band_id, genre_id)
+--VALUES (, );
 
 
 COMMIT;
